a6af55b338b617575068951215083e82711ff86f
Luiz Madrid <luizam987@gmail.com>
2023-09-07 19:49:50 -0300

    Implementado o botão de apagar.

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const novoItemInput = document.getElementById("input-novo-item");
   const btnAdicionarItem = document.getElementsByClassName("btn-adicionar")[0];
   const listaItens = document.getElementsByTagName("ul")[0];
+  const botoesApagar = document.getElementsByClassName("btn-apagar");
+
+  /* Botão de adicionar elemento ficar inutilizado quando o mesmo não tiver caracteres */
+
+  novoItemInput.addEventListener("input", () => {
+    if (novoItemInput.value != "") {
+      btnAdicionarItem.classList.remove("desabilitado");
+      btnAdicionarItem.disabled = false;
+    } else {
+      btnAdicionarItem.classList.add("desabilitado");
+      btnAdicionarItem.disabled = true;
+    }
+  });
+
+  /* Botão de adicionar elemento ficar inutilizado quando o mesmo não tiver caracteres */
+
+  /* Adicionar elementos na lista com [createElement] */
 
   btnAdicionarItem.addEventListener("click", (e) => {
     e.preventDefault();
@@ -25,6 +42,12 @@ document.addEventListener("DOMContentLoaded", () => {
     botaoEditar.classList.add("btn-editar");
     botaoApagar.classList.add("btn-apagar");
 
+    botaoApagar.addEventListener("click", () => {
+      botaoApagar.parentNode.parentNode.remove();
+    });
+
+    containerBotoes.classList.add("container-botoes");
+
     containerBotoes.append(botaoEditar);
     containerBotoes.append(botaoApagar);
 
@@ -33,5 +56,20 @@ document.addEventListener("DOMContentLoaded", () => {
     listaItens.append(item);
 
     novoItemInput.value = "";
+
+    btnAdicionarItem.disabled = true;
+    btnAdicionarItem.classList.add("desabilitado");
   }
+
+  /* Adicionar elementos na lista com [createElement] */
+
+  /* Definindo a funcionalidade de APAGAR um item */
+
+  for (let botao of botoesApagar) {
+    botao.addEventListener("click", () => {
+      botao.parentNode.parentNode.remove();
+    });
+  }
+
+  /* Definindo a funcionalidade de APAGAR um item */
 });
