ec69e63b9e50740d92634315f14918b8af3225d1
Luiz Madrid <luizam987@gmail.com>
2023-09-07 21:54:52 -0300

    Feito edição e cancelar edição.

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const btnAdicionarItem = document.getElementsByClassName("btn-adicionar")[0];
   const listaItens = document.getElementsByTagName("ul")[0];
   const botoesApagar = document.getElementsByClassName("btn-apagar");
+  const botoesEditar = document.getElementsByClassName("btn-editar");
+  const botoesSalvar = document.getElementsByClassName("btn-salvar");
 
-  /* Botão de adicionar elemento ficar inutilizado quando o mesmo não tiver caracteres */
+  /* Inicio do botão de adicionar elemento ficar inutilizado quando o mesmo não tiver caracteres */
 
   novoItemInput.addEventListener("input", () => {
     if (novoItemInput.value != "") {
@@ -16,9 +18,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  /* Botão de adicionar elemento ficar inutilizado quando o mesmo não tiver caracteres */
+  /* Fim do botão de adicionar elemento ficar inutilizado quando o mesmo não tiver caracteres */
 
-  /* Adicionar elementos na lista com [createElement] */
+  /* ===================!!!!=================== */
+
+  /* Começo da sessão de adicionar elementos na lista com [createElement] */
 
   btnAdicionarItem.addEventListener("click", (e) => {
     e.preventDefault();
@@ -32,6 +36,8 @@ document.addEventListener("DOMContentLoaded", () => {
     msg.disabled = true;
     msg.value = texto;
 
+    /* Começo da criação dos botões EDITAR e APAGAR */
+
     const containerBotoes = document.createElement("div");
     const botaoEditar = document.createElement("button");
     const botaoApagar = document.createElement("button");
@@ -42,17 +48,90 @@ document.addEventListener("DOMContentLoaded", () => {
     botaoEditar.classList.add("btn-editar");
     botaoApagar.classList.add("btn-apagar");
 
-    botaoApagar.addEventListener("click", () => {
-      botaoApagar.parentNode.parentNode.remove();
-    });
-
     containerBotoes.classList.add("container-botoes");
 
     containerBotoes.append(botaoEditar);
     containerBotoes.append(botaoApagar);
 
+    /* Fim da criação dos botões EDITAR e APAGAR */
+    /* ===================!!!!=================== */
+    /* Começo da criação dos botões SALVAR e CANCELAR */
+
+    const containerBotoesSalvarCancelar = document.createElement("div");
+    const botaoSalvar = document.createElement("button");
+    const botaoCancelar = document.createElement("button");
+
+    botaoSalvar.innerText = "Salvar";
+    botaoCancelar.innerText = "Cancelar";
+
+    botaoSalvar.classList.add("btn-salvar");
+    botaoCancelar.classList.add("btn-cancelar");
+
+    containerBotoesSalvarCancelar.classList.add("container-botoes-salvar-cancelar", "esconder-botoes");
+
+    containerBotoesSalvarCancelar.append(botaoSalvar);
+    containerBotoesSalvarCancelar.append(botaoCancelar);
+
+    /* Fim da criação dos botões SALVAR e CANCELAR */
+    /* ===================!!!!=================== */
+    /* Começo da criação do botão de REMOVER o elemento */
+
+    botaoApagar.addEventListener("click", () => {
+      botaoApagar.parentNode.parentNode.remove();
+    });
+
+    /* Fim da criação do botão de REMOVER o elemento */
+    /* ===================!!!!=================== */
+    /* Começo da criação do botão de EDITAR o elemento */
+
+    botaoEditar.addEventListener("click", () => {
+      const input = botaoEditar.parentNode.parentNode.getElementsByTagName("input")[0];
+      input.disabled = false;
+
+      input.setSelectionRange(0, 100);
+      input.focus();
+
+      const botoesSalvarCancelar = botaoEditar.parentNode.parentNode.getElementsByClassName("container-botoes-salvar-cancelar")[0];
+
+      botoesSalvarCancelar.classList.remove("esconder-botoes");
+      botaoEditar.parentNode.classList.add("esconder-botoes");
+
+      const conteudoItem = input.value;
+
+      /* ===================!!!!=================== */
+      /* Começo do botão CANCELAR o elemento */
+
+      botaoCancelar.addEventListener("click", () => {
+        input.value = conteudoItem;
+        input.disabled = true;
+
+        botoesSalvarCancelar.classList.add("esconder-botoes");
+        botaoEditar.parentNode.classList.remove("esconder-botoes");
+      });
+    });
+
+    /* Fim do botão CANCELAR o elemento */
+    /* ===================!!!!=================== */
+    /* Começo do botão SALVAR o elemento */
+
+    botaoSalvar.addEventListener("click", () => {
+      const input = botaoSalvar.parentNode.parentNode.getElementsByTagName("input")[0];
+
+      input.disabled = true;
+
+      const botoesEditarApagar = botaoSalvar.parentNode.parentNode.getElementsByClassName("container-botoes")[0];
+
+      botaoSalvar.parentNode.classList.add("esconder-botoes");
+      botoesEditarApagar.classList.remove("esconder-botoes");
+    });
+
+    /* Fim do botão SALVAR o elemento */
+    /* ===================!!!!=================== */
+    /* Fim da criação do botão de EDITAR o elemento */
+
     item.append(msg);
     item.append(containerBotoes);
+    item.append(containerBotoesSalvarCancelar);
     listaItens.append(item);
 
     novoItemInput.value = "";
@@ -61,9 +140,11 @@ document.addEventListener("DOMContentLoaded", () => {
     btnAdicionarItem.classList.add("desabilitado");
   }
 
-  /* Adicionar elementos na lista com [createElement] */
+  /* Fim da sessão de adicionar elementos na lista com [createElement] */
 
-  /* Definindo a funcionalidade de APAGAR um item */
+  /* ===================!!!!=================== */
+
+  /* Começo da definição da funcionalidade de APAGAR um item */
 
   for (let botao of botoesApagar) {
     botao.addEventListener("click", () => {
@@ -71,5 +152,61 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  /* Definindo a funcionalidade de APAGAR um item */
+  /* Fim da definição da funcionalidade de APAGAR um item */
+
+  /* ===================!!!!=================== */
+
+  /* Começo da definição da funcionalidade de EDITAR um item */
+
+  for (let botao of botoesEditar) {
+    botao.addEventListener("click", () => {
+      const input = botao.parentNode.parentNode.getElementsByTagName("input")[0];
+      input.disabled = false;
+
+      input.setSelectionRange(0, 100);
+      input.focus();
+
+      const botoesSalvarCancelar = botao.parentNode.parentNode.getElementsByClassName("container-botoes-salvar-cancelar")[0];
+
+      botoesSalvarCancelar.classList.remove("esconder-botoes");
+      botao.parentNode.classList.add("esconder-botoes");
+
+      const conteudoItem = input.value;
+
+      /* Começo do botão CANCELAR o elemento */
+
+      const botaoCancelar = botoesSalvarCancelar.getElementsByClassName("btn-cancelar")[0];
+
+      botaoCancelar.addEventListener("click", () => {
+        input.value = conteudoItem;
+        input.disabled = true;
+
+        botoesSalvarCancelar.classList.add("esconder-botoes");
+        botao.parentNode.classList.remove("esconder-botoes");
+      });
+
+      /* Fim do botão CANCELAR o elemento */
+    });
+  }
+
+  /* Fim da definição da funcionalidade de EDITAR um item */
+
+  /* ===================!!!!=================== */
+
+  /* Começo do botão SALVAR o elemento */
+
+  for (let botao of botoesSalvar) {
+    botao.addEventListener("click", () => {
+      const input = botao.parentNode.parentNode.getElementsByTagName("input")[0];
+
+      input.disabled = true;
+
+      const botoesEditarApagar = botao.parentNode.parentNode.getElementsByClassName("container-botoes")[0];
+
+      botao.parentNode.classList.add("esconder-botoes");
+      botoesEditarApagar.classList.remove("esconder-botoes");
+    });
+
+    /* Fim do botão SALVAR o elemento */
+  }
 });
